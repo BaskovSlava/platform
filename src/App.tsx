a91fbfbfe23fba0c1903c3dc2f5bacb78141ab55
SlavaBaskov <bozhe4uk@gmail.com>
2024-04-23 23:37:00 +0300

    scss .module. react

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.scss'
+import Counter from './components/Counter';
+
+export const App = () => {
+  return (
+    <div className='app'>
+        <h1>sdfsdffa</h1>
+        <Counter/>
+    </div>
+  )
+}
+
+export default App;
