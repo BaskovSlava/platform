9efc3c060a97c47334992ff4d954b8bf6b9a3769
SlavaBaskov <bozhe4uk@gmail.com>
2024-04-25 19:32:14 +0300

    Routing Lazy Suspence Theme LocalStorege

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import './index.scss'
-import Counter from './components/Counter';
+import "./styles/index.scss";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense, useContext, useState } from "react";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+  const {theme, toggleTheme} = useTheme();
   return (
-    <div className='app'>
-        <h1>sdfsdffa</h1>
-        <Counter/>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>toggle</button>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О нас</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
 export default App;
